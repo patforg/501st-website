f2553d64612d98c8ecc2bd657bd040a2d306e3a9
Patrick Forget <pforget@focuslabs.com>
2024-06-07 16:40:04 -0400

    updated base

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -4,7 +4,7 @@ import { defineConfig } from 'vitepress'
 export default defineConfig({
   title: "501st Legion Alliance",
   description: "Tips and tricks for LastWar",
-  base: '/501st-website/',
+  base: '/',
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
